28cd417bf31b48770ecd11879aae5d731aec66bc
Volin <omnipresentcode@gmail.com>
2022-04-08 15:00:12 -0500

    initial commit

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include    <cstdio>
+#include	<stdlib.h>
+#include    <SDL2/SDL.h>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  
+ * =====================================================================================
+ */
+    int
+main ( int argc, char *argv[] )
+{
+    //The window we'll be rendering to
+    SDL_Window* window = NULL;
+
+    //The surface contained by the window
+    SDL_Surface* screenSurface = NULL;
+
+    //Initialize SDL
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    {
+        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+    }
+    else
+    {
+        //Create Window
+        window = SDL_CreateWindow( "cnake", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        if( window == NULL )
+        {
+            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+        }
+        else
+        {
+            //Get window surface
+//            screenSurface = SDL_GetWindowSurface( window );
+
+            //Fill the surface white
+//            SDL_FillRect( screenSurface, NULL, SDL_MapRGB(
+        }
+    }
+
+
+    return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
